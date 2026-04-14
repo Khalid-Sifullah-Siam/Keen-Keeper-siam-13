da14a8b6dc946d2805afa2d1ac7031419a0bdf9c
KhalidSifullahSIam2004 <khalidsiam1754@gmail.com>
2026-04-14 14:41:32 +0600

    stats page build done

---
+++ src/Components/Friends/Friends.jsx
@@ -0,0 +1,26 @@
+"use client"
+import useFriends from '@/Hooks/useFriends';
+import FriendCard from './FriendCard';
+import Loading from '../Loading/Loading';
+
+
+
+const Friends = () => {
+    const { friends, loading } = useFriends()
+
+    if (loading) return <Loading />
+
+
+    return (
+        <div className='mt-10'>
+            <h1 className='text-3xl font-bold'>your Friends</h1>
+            <div className='grid md:grid-cols-3 lg:grid-cols-4'>
+                {
+                    friends.map(friend => <FriendCard key={friend.id} friend={friend} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Friends;
